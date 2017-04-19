ea270a1af06932c549e8d40d6674b42c5315f459
Jamie  Giuliano <jamiepgiuliano@gmail.com>
2017-04-19 00:06:21 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,29 @@
+var array = [];
+
+function forLoop (array) {
+  for (let i = 0; i < 25; i++) {
+    array.push(`I am ${i} strange loops.`)
+  }
+  return array;
+}
+forLoop(array);
+
+function whileLoop(n) {
+  let countdown = n
+
+  while (countdown > 0) {
+    console.log(--countdown)
+
+  }  return "done";
+
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+  do {
+    array.pop();
+  } while (array.length > 0 && maybeTrue());
+  return array
+}
